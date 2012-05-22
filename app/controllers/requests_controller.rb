32fd4520e6eb9677c9cfc35952517a21d13b4323
SmitBLR <smitblr@gmail.com>
2012-05-22 10:49:44 +0300

    Last request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -14,4 +14,12 @@ class RequestsController < ApplicationController
     @movies = Movie.joins(:movie_genres).where(["movie_genres.percentage >= ?", params[:percentage_from]]).uniq
     render "movies/index"
   end
+
+  def new_involve_search
+  end
+
+  def show_involve_search
+    @actors = Actor.joins(:movie_actors).where(["movie_actors.involve_id = ?", params[:actor][:involve]]).uniq
+    render "actors/index"
+  end
 end
